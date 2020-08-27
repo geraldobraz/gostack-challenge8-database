e601edfe8b53c56238f2cf86422226033692a95d
geraldobraz <gban50@gmail.com>
2020-08-27 08:52:57 -0300

    Implement functionalities

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,48 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('Custumer does not exists.');
+    }
+
+    const foundProducts = await this.productsRepository.findAllById(products);
+    if (foundProducts.length !== products.length) {
+      throw new AppError('Invalid products');
+    }
+
+    for (let index = 0; index < foundProducts.length; index += 1) {
+      if (products[index].quantity > foundProducts[index].quantity) {
+        throw new AppError('Product with insuficient quantities');
+      }
+    }
+
+    const formatedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: foundProducts.filter(
+        foundProduct => foundProduct.id === product.id,
+      )[0].price,
+    }));
+    const order = await this.ordersRepository.create({
+      customer,
+      products: formatedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,52 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+    const foundProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return foundProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+    const foundProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    for (let index = 0; index < foundProducts.length; index += 1) {
+      foundProducts[index].quantity -= products[index].quantity;
+    }
+
+    return this.ormRepository.save(foundProducts);
   }
 }
 
